93b13287afebe05ab4ea02c69472482072d3102d
Seth Todd <seth.todd@outlook.com>
2020-05-05 00:55:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,19 +13,15 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each {|students| Student.new(students)}
-    
   end
 
   def add_student_attributes(attributes_hash)
     attributes_hash.each {|key, value| self.send("#{key}=",  value)}
     self
-  
-    binding.pry
-    
   end
 
   def self.all
-    
+    @@all
   end
 end
 
